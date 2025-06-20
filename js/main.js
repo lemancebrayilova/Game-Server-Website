feddfa4e130cd09ce1c37ce6f9d0cc57bb6239e0
lemancebrayilova <cbrayilovalman33@gmail.com>
2025-06-20 14:29:57 +0300

    home section style and swiper

--- js/main.js
+++ js/main.js
@@ -13,3 +13,17 @@ let menu = document.querySelector('.menu-icon')
 menu.onclick = () => {
     menu.classList.toggle("move")
 }
+
+// Home Swiper
+var swiper = new Swiper(".home", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 25000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
