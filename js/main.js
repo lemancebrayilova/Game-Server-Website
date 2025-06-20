452a8ba39a64186b260468e861714418f34316dc
lemancebrayilova <cbrayilovalman33@gmail.com>
2025-06-20 13:46:10 +0300

    add menu toggle icon

--- js/main.js
+++ js/main.js
@@ -5,3 +5,11 @@ let dropLogin = document.querySelector('.drop-login')
 loginBtn.onclick = () => {
     dropLogin.classList.toggle("drop-login-open")
 }
+
+// Menu Open Close
+let menu = document.querySelector('.menu-icon')
+// let navbar = document.querySelector('.navbar')
+
+menu.onclick = () => {
+    menu.classList.toggle("move")
+}
