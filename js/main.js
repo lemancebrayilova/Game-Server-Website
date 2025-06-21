7510684ae6d1249519fbe400b0a69bae546653cc
lemancebrayilova <cbrayilovalman33@gmail.com>
2025-06-21 22:20:57 +0300

    add accordion toggle

--- js/main.js
+++ js/main.js
@@ -27,3 +27,32 @@ var swiper = new Swiper(".home", {
         clickable: true,
     },
 });
+
+// Accordion
+const accordionItems = document.querySelectorAll('.accordion-item');
+
+accordionItems.forEach((item) => {
+    const accordionHeader = item.querySelector('.accordion-header');
+
+    accordionHeader.addEventListener('click', () => {
+        const openItem = document.querySelector('.accordion-open');
+
+        toggleItem(item);
+
+        if(openItem && openItem !== item) {
+            toggleItem(openItem);
+        }
+    })
+})
+
+const toggleItem = (item) => {
+    const accordionContent = item.querySelector(".accordion-content");
+
+    if(item.classList.contains("accordion-open")) {
+        accordionContent.removeAttribute("style");
+        item.classList.remove("accordion-open");
+    } else {
+        accordionContent.style.height = accordionContent.scrollHeight + "px";
+        item.classList.add("accordion-open");
+    }
+};
